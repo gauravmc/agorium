cf830fb95e6c2033b097c14e4cd6cdfa051ba77d
Gaurav Chande <gmail@gauravchande.com>
2019-02-16 01:11:40 +0530

    Setup HomeController

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Agorium
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.helper      false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
