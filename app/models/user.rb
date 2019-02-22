57889146c47b9ef87a0cb2b7124f277e69595f9f
Gaurav Chande <gmail@gauravchande.com>
2019-02-22 18:26:06 +0530

    Update User validation: regex can check length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  validates :phone, format: { with: /\A\d+\z/, message: "should be a 10-digit number" }
+  validates :phone, format: { with: /\A\d{10}\z/, message: "should be a 10-digit number" }
   validates :phone, uniqueness: true
-
-  # :if condition because otherwise Rails unnecessarily
-  # validates length even when phone is something non-numeric,
-  # creating two errors messages, which is bad UX. Wish
-  # there was a better way to do this.
-  validates :phone, length: { is: 10, wrong_length: 'should be a 10-digit number' }, if: Proc.new {|u| u.errors[:phone].none?}
 
   def phone_successfully_verified!
     update_attribute(:phone_verified, true)
