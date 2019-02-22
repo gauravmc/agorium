f6419760898c97c8eaf10f52beb5c3dbaa448509
Gaurav Chande <gmail@gauravchande.com>
2019-02-22 12:02:54 +0530

    Add regression tests for users create & verify xhr requests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -20,12 +20,31 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_match "Phone should be a 10-digit number", @response.body
   end
 
+  test "xhr request to create action responds correctly when user cannot be created" do
+    assert_no_difference('User.count') do
+      post users_url, params: { user: { name: 'Dibs', phone: 'invalid' } }, xhr: true
+    end
+
+    assert_response :unprocessable_entity
+    assert_match "Phone should be a 10-digit number", @response.body
+  end
+
   test "should redirect to phone verification page once user is created" do
     assert_difference('User.count') do
       post users_url, params: { user: { name: 'Dibs', phone: '9800098000' } }
     end
 
     assert_redirected_to user_verify_path(User.last.id)
+  end
+
+  test "xhr request to create action responds correctly when user is created" do
+    assert_difference('User.count') do
+      post users_url, params: { user: { name: 'Dibs', phone: '9800098000' } }, xhr: true
+    end
+
+    assert_response :success
+    assert_match "Turbolinks.visit", @response.body
+    assert_match user_verify_path(User.last.id), @response.body
   end
 
   test "verify renders otp entering page" do
@@ -43,11 +62,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_match "OTP did not match, please try entering again", @response.body
   end
 
+  test "xhr request should render errors when otp cannot be verified" do
+    new_user = User.create!(name: 'Garr', phone: '9604884000')
+    put user_check_otp_url(new_user.id), params: { otp: 'invalid' }, xhr: true
+
+    assert_response :unprocessable_entity
+    assert_match "OTP did not match, please try entering again", @response.body
+  end
+
   test "should update user record and redirect to home once otp is good" do
     new_user = User.create!(name: 'Garr', phone: '9604884000')
     put user_check_otp_url(new_user.id), params: { otp: '420042' }
 
     assert new_user.reload.phone_verified?
     assert_redirected_to root_path
+  end
+
+  test "xhr request should update user record and redirect to home once otp is good" do
+    new_user = User.create!(name: 'Garr', phone: '9604884000')
+    put user_check_otp_url(new_user.id), params: { otp: '420042' }, xhr: true
+
+    assert new_user.reload.phone_verified?
+    assert_match "Turbolinks.visit", @response.body
+    assert_match root_path, @response.body
   end
 end
