10c72006fa403554e24d299a97a00af818a25644
Gaurav Chande <gmail@gauravchande.com>
2019-02-28 00:11:03 +0530

    Phew, fix layouts and create two-sided admin layout

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,29 +1,9 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "section" ]
+  static targets = [ "notification" ]
 
   remove(event) {
-    this.removeNotification(event.target.parentNode)
-
-    if (this.getFlashCount() === 0)
-      this.removeFlashSection()
-  }
-
-  removeNotification(notification) {
-    notification.parentNode.removeChild(notification)
-    this.decrementFlashCount()
-  }
-
-  removeFlashSection() {
-    this.sectionTarget.parentNode.removeChild(this.sectionTarget)
-  }
-
-  decrementFlashCount() {
-    this.sectionTarget.dataset.flashCount = this.getFlashCount() - 1
-  }
-
-  getFlashCount() {
-    return parseInt(this.data.get('count'))
+    this.notificationTarget.parentNode.removeChild(this.notificationTarget)
   }
 }
