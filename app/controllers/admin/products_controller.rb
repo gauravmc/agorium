f015e993bc0366cc4e0ae9060eb395a455930b24
Gaurav Chande <gmail@gauravchande.com>
2019-03-01 17:30:28 +0530

    Complete async product creation end-point

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,4 +2,25 @@ class Admin::ProductsController < AdminController
   def new
     @product = current_user.products.new
   end
+
+  def create
+    @product = current_user.products.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        flash[:success] = "#{@product.name} was successfully added to your products!"
+        format.html { redirect_to new_admin_product_path }
+      else
+        flash.now[:danger] = "Product could not be created because of some input errors."
+        format.html { render :new, status: :unprocessable_entity }
+        format.js { render :create, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :cost, :inventory)
+  end
 end
--- test/controllers/admin/products_controller_test.rb
+++ test/controllers/admin/products_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class Admin
   class ProductsControllerTest < ActionDispatch::IntegrationTest
     setup do
-      log_in_as(users(:shawn))
+      @user = users(:shawn)
+      log_in_as(@user)
     end
 
     test "new renders new product page" do
@@ -11,6 +12,58 @@ class Admin
 
       assert_response :success
       assert_match 'Add a new Product', @response.body
+    end
+
+    test "create renders form with errors when product is invalid" do
+      params = new_product_params
+      params[:price] = ""
+
+      assert_no_difference('Product.count') do
+        post admin_products_url, params: { product: params }
+      end
+
+      assert_response :unprocessable_entity
+      assert_match "Selling price is not a number", @response.body
+
+      assert_no_difference('Product.count') do
+        post admin_products_url, params: { product: params }, xhr: true
+      end
+
+      assert_response :unprocessable_entity
+      assert_equal "text/javascript", @response.content_type
+      assert_match "Selling price is not a number", @response.body
+    end
+
+    test "create redirects with success message when product is valid" do
+      assert_difference('Product.count') do
+        post admin_products_url, params: { product: new_product_params }
+      end
+
+      assert_equal "#{new_product_params[:name]} was successfully added to your products!", flash[:success]
+      assert_redirected_to new_admin_product_url
+      assert_equal new_product_params[:name], @user.products.last.name
+
+      assert_difference('Product.count') do
+        post admin_products_url, params: { product: new_product_params }, xhr: true
+      end
+
+      assert_response :success
+      assert_equal "text/javascript", @response.content_type
+      assert_equal "#{new_product_params[:name]} was successfully added to your products!", flash[:success]
+      assert_match new_admin_product_url, @response.body
+      assert_equal new_product_params[:name], @user.products.last.name
+    end
+
+    private
+
+    def new_product_params
+      {
+        name: "Cool New Product",
+        description: "Cool New Product is very cool.",
+        price: 42.24,
+        cost: 24.42,
+        inventory: 42
+      }
     end
   end
 end
