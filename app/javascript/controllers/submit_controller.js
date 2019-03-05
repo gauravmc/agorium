9dd05441234bb1cf85440175bd0e9c75b22c60ef
Gaurav Chande <gmail@gauravchande.com>
2019-03-05 19:26:58 +0530

    Fix: Form submit buttons spinning when HTML5's native validation failed

--- app/javascript/controllers/submit_controller.js
+++ app/javascript/controllers/submit_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "button" ]
+  static targets = [ "button", "form" ]
 
   process(event) {
-    this.buttonTarget.classList.add('is-loading')
+    if (this.formTarget.checkValidity())
+      this.buttonTarget.classList.add('is-loading')
   }
 }
