338c5267918d0b5966702c6c33ad1f3666734098
Gaurav Chande <gmail@gauravchande.com>
2019-03-08 04:37:58 +0530

    Completely revamp the branding with new logo, colours, and fonts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'stylesheets/application';
+
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import { Application } from "stimulus"
