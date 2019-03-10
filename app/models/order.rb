10634c4700be1969880d77a6a58901248090c86d
Gaurav Chande <gmail@gauravchande.com>
2019-03-10 19:27:13 +0530

    Decrement product inventory when order gets generated

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,6 +10,8 @@ class Order < ApplicationRecord
     cancelled: 'cancelled'
   }
 
+  class ProductOutOfStockError < StandardError; end
+
   def self.generate(customer, cart)
     Order.transaction do
       customer.save
@@ -22,8 +24,22 @@ class Order < ApplicationRecord
       order.save!
 
       cart.transfer_items_to_order(order)
+      order.update_products_inventory
 
       return order
+    end
+  end
+
+  def update_products_inventory
+    line_items.preload(:product).each do |line_item|
+      product = line_item.product
+      product.decrement(:inventory, line_item.quantity)
+
+      if product.inventory < 0
+        raise ProductOutOfStockError, "Product product_id=#{product.id} could not be added to Order order_id=#{id} because new inventory is becoming less than 0."
+      else
+        product.save(validate: false)
+      end
     end
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -48,7 +48,7 @@ class OrderTest < ActiveSupport::TestCase
     assert_equal 171.96, order.total_price
   end
 
-  test ".generate cart line items get transferred to the order" do
+  test "cart line items get transferred to the order when it is generated" do
     customer = Customer.new(name: 'Random dude', phone: '8767876534')
     cart = carts(:maple_cart)
     line_item_ids = cart.line_items.map(&:id)
@@ -59,5 +59,24 @@ class OrderTest < ActiveSupport::TestCase
     line_items = LineItem.where(id: line_item_ids)
     assert_equal [nil, nil], line_items.map(&:cart)
     assert_equal [order, order], line_items.map(&:order)
+  end
+
+  test ".generate raises an error if product goes out of stock while creating order" do
+    product = products(:summer_butter)
+    product.update_column(:inventory, 2)
+
+    assert_raise(Order::ProductOutOfStockError) do
+      Order.generate(customers(:vin), carts(:maple_cart))
+    end
+  end
+
+  test "products' inventory gets updated after order generation" do
+    product_1 = products(:summer_butter)
+    product_2 = products(:fire_balm)
+
+    Order.generate(customers(:vin), carts(:maple_cart))
+
+    assert_equal 17, product_1.reload.inventory
+    assert_equal 29, product_2.reload.inventory
   end
 end
